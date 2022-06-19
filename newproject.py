ce998c72e563265896b99eedd7c28e9edebf737e
francesco <francescosacco.github@gmail.com>
2022-06-19 23:52:53 +0200

    Set Up the Initialization

--- newproject.py
+++ newproject.py
@@ -4,3 +4,10 @@ from github import Github
 from dotenv import load_dotenv
 import sys
 import os
+
+# Initialization
+# Loading the ENV Variables
+load_dotenv()
+
+# Creating a Global Console Object
+CONSOLE = Console()
